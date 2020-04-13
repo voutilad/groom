1f56f2848f13fce6fa3d33e7aecb01712d295a9e
Dave Voutila <voutilad@gmail.com>
2020-04-13 17:59:22 -0400

    tabs -> spaces ;-)

--- src/main/java/io/sisu/groom/GroomApplication.java
+++ src/main/java/io/sisu/groom/GroomApplication.java
@@ -15,48 +15,48 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GroomApplication {
-	private static final Duration WINDOW_DURATION = Duration.ofSeconds(5);
-	private static final int WINDOW_SIZE = 200;
+    private static final Duration WINDOW_DURATION = Duration.ofSeconds(5);
+    private static final int WINDOW_SIZE = 200;
 
-	// Set up nicer logging output.
-	private static final Logger logger;
-	static {
-		System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
-		System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "[yyyy-MM-dd'T'HH:mm:ss:SSS]");
-		System.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
-		logger = LoggerFactory.getLogger(GroomApplication.class);
-	}
+    // Set up nicer logging output.
+    private static final Logger logger;
+    static {
+        System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
+        System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "[yyyy-MM-dd'T'HH:mm:ss:SSS]");
+        System.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
+        logger = LoggerFactory.getLogger(GroomApplication.class);
+    }
 
-	public static void main(String[] args) throws Exception {
-		logger.info("GROOM STARTING!");
+    public static void main(String[] args) throws Exception {
+        logger.info("GROOM STARTING!");
 
-		try (Database db = new Database(Database.defaultConfig, "neo4j", "password")) {
-			Arrays.stream(Cypher.SCHEMA_QUERIES).forEach(query -> {
-				try {
-					db.write(new Query(query)).blockLast(Duration.ofSeconds(5));
-				} catch (Exception e) {
-					System.err.println("Could not execute " + query);
-				}
-			});
+        try (Database db = new Database(Database.defaultConfig, "neo4j", "password")) {
+            Arrays.stream(Cypher.SCHEMA_QUERIES).forEach(query -> {
+                try {
+                    db.write(new Query(query)).blockLast(Duration.ofSeconds(5));
+                } catch (Exception e) {
+                    System.err.println("Could not execute " + query);
+                }
+            });
 
-			Connection conn = UdpServer.create()
-					.host("0.0.0.0")
-					.port(10666)
-					.handle((in, out) ->
-							in.receive().asString()
-									//.log("RAWJSON")
-									.flatMap(Event::fromJson)
-									.windowTimeout(WINDOW_SIZE, WINDOW_DURATION)
-									.map(windowFlux ->
-											windowFlux.collectSortedList(Comparator.comparing(Event::getFrame))
-													.map(Cypher::compileBulkEventComponentInsert)
-													.map(query -> db.write(query).subscribe())
-													.subscribe())
-									.then())
-					.doOnBound(connection -> logger.info("READY!!!"))
-					.bindNow(Duration.ofSeconds(5));
-			conn.onDispose().block();
-		}
-		logger.info("GROOM DONE!");
-	}
+            Connection conn = UdpServer.create()
+                    .host("0.0.0.0")
+                    .port(10666)
+                    .handle((in, out) ->
+                            in.receive().asString()
+                                    //.log("RAWJSON")
+                                    .flatMap(Event::fromJson)
+                                    .windowTimeout(WINDOW_SIZE, WINDOW_DURATION)
+                                    .map(windowFlux ->
+                                            windowFlux.collectSortedList(Comparator.comparing(Event::getFrame))
+                                                    .map(Cypher::compileBulkEventComponentInsert)
+                                                    .map(query -> db.write(query).subscribe())
+                                                    .subscribe())
+                                    .then())
+                    .doOnBound(connection -> logger.info("READY!!!"))
+                    .bindNow(Duration.ofSeconds(5));
+            conn.onDispose().block();
+        }
+        logger.info("GROOM DONE!");
+    }
 }
