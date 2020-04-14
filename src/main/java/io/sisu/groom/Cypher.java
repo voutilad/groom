fdb700af18376ac7cdc663ddb2f6b8b52a07b9eb
Dave Voutila <voutilad@gmail.com>
2020-04-14 07:40:49 -0400

    Let the data flow! UDP plumbing is working.
    Finally got what seems to be a viable config going for getting the data
    to flow from in UDP listener to the Database with windowing going on in
    between. (Question now is...do we really need to order the data?)
    
    Still lacks the threading, but that's the next part. Graph components at
    least being created around Events.
    
    Also fixes some schema setup issues found along the way.
    
    Minor change: bind to 127.0.0.1 and not 0.0.0.0...for some reason that
    wasn't working on Windows, plus we shouldn't bind on external facing
    adapters anyways without consent ;-)

--- src/main/java/io/sisu/groom/Cypher.java
+++ src/main/java/io/sisu/groom/Cypher.java
@@ -9,28 +9,28 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Cypher {
-    public static final String[] SCHEMA_QUERIES = {
-            "CREATE CONSTRAINT ON (n:Mission) ASSERT n.id IS UNIQUE",
-            "CREATE CONSTRAINT ON (n:Frame) ASSERT n.tic IS UNIQUE",
-            "CREATE CONSTRAINT ON (n:Level) ASSERT n.id IS UNIQUE",
-            "CREATE CONSTRAINT ON (n:Actor) ASSERT n.id IS UNIQUE",
-            "CREATE CONSTRAINT ON (n:SubSector) ASSERT n.id IS UNIQUE",
+  public static final String[] SCHEMA_QUERIES = {
+    "CREATE CONSTRAINT ON (n:Mission) ASSERT n.id IS UNIQUE",
+    "CREATE CONSTRAINT ON (n:Frame) ASSERT n.tic IS UNIQUE",
+    "CREATE CONSTRAINT ON (n:Level) ASSERT n.id IS UNIQUE",
+    "CREATE CONSTRAINT ON (n:Actor) ASSERT n.id IS UNIQUE",
+    "CREATE CONSTRAINT ON (n:SubSector) ASSERT n.id IS UNIQUE",
+    "CREATE INDEX ON :Frame(millis)",
+    "CREATE INDEX ON :Actor(type)",
+    "CREATE INDEX ON :Enemy(type)",
+    "CREATE INDEX ON :State(position)",
+    "CREATE INDEX ON :PlayerState(position)",
+    "CREATE INDEX ON :EnemyState(position)",
+    "CREATE INDEX ON :State(actorId)",
+    "CREATE INDEX ON :PlayerState(actorId)",
+    "CREATE INDEX ON :EnemyState(actorId)",
+  };
 
-            // "CREATE CONSTRAINT ON (n:Event) ASSERT exists(n.type)",
-
-            "CREATE INDEX ON :Frame(millis)",
-            "CREATE INDEX ON :Actor(type)",
-            "CREATE INDEX ON :State(position)",
-            "CREATE INDEX ON :PlayerState(position)",
-            "CREATE INDEX ON :EnemyState(position)",
-            "CREATE INDEX ON :State(actorId)",
-            "CREATE INDEX ON :PlayerState(actorId)",
-            "CREATE INDEX ON :EnemyState(actorId)",
-            "CREATE INDEX ON :Actor:Enemy(type)",
-    };
-
-    private static final String UNWIND_PARAM = "eventList";
-    private static final String BULK_EVENT_INSERT_CYPHER = String.join("\n", new String[] {
+  private static final String UNWIND_PARAM = "eventList";
+  private static final String BULK_EVENT_INSERT_CYPHER =
+      String.join(
+          "\n",
+          new String[] {
             // Bulk construct event components
             "UNWIND $" + UNWIND_PARAM + " AS event",
 
@@ -56,13 +56,11 @@ public class Cypher {
             "    FOREACH (_ IN CASE thing.type WHEN 'player' THEN [1] ELSE [] END | SET actor:Player, actorState:PlayerState)",
             "    FOREACH (_ IN CASE thing.type WHEN 'player' THEN [] ELSE [1] END | SET actor:Enemy, actorState:EnemyState)",
             ")",
+          });
 
-    });
-
-    public static Query compileBulkEventComponentInsert(List<Event> events) {
-        Map<String, Object> params = new HashMap();
-        params.put(UNWIND_PARAM, events.stream().map(Event::toMap).collect(Collectors.toList()));
-        return new Query(BULK_EVENT_INSERT_CYPHER, params);
-    }
-
+  public static Query compileBulkEventComponentInsert(List<Event> events) {
+    Map<String, Object> params = new HashMap();
+    params.put(UNWIND_PARAM, events.stream().map(Event::toMap).collect(Collectors.toList()));
+    return new Query(BULK_EVENT_INSERT_CYPHER, params);
+  }
 }
--- src/main/java/io/sisu/groom/GroomApplication.java
+++ src/main/java/io/sisu/groom/GroomApplication.java
@@ -2,61 +2,82 @@ package io.sisu.groom;
 
 import io.sisu.groom.events.Event;
 import org.neo4j.driver.Query;
+import org.neo4j.driver.exceptions.ClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
+import org.slf4j.MarkerFactory;
 import reactor.netty.Connection;
 import reactor.netty.udp.UdpServer;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class GroomApplication {
-    private static final Duration WINDOW_DURATION = Duration.ofSeconds(5);
-    private static final int WINDOW_SIZE = 200;
+  private static final Duration WINDOW_DURATION = Duration.ofSeconds(5);
+  private static final int WINDOW_SIZE = 500;
 
-    // Set up nicer logging output.
-    private static final Logger logger;
-    static {
-        System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
-        System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "[yyyy-MM-dd'T'HH:mm:ss:SSS]");
-        System.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
-        logger = LoggerFactory.getLogger(GroomApplication.class);
-    }
+  // Set up nicer logging output.
+  private static final Logger logger;
 
-    public static void main(String[] args) throws Exception {
-        logger.info("GROOM STARTING!");
+  static {
+    System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
+    System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "[yyyy-MM-dd'T'HH:mm:ss:SSS]");
+    System.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
+    logger = LoggerFactory.getLogger(GroomApplication.class);
+  }
 
-        try (Database db = new Database(Database.defaultConfig, "neo4j", "password")) {
-            Arrays.stream(Cypher.SCHEMA_QUERIES).forEach(query -> {
+  public static void main(String[] args) throws Exception {
+    logger.info("GROOM STARTING!");
+
+    try (Database db = new Database(Database.defaultConfig, "neo4j", "password")) {
+      Arrays.stream(Cypher.SCHEMA_QUERIES)
+          .forEach(
+              query -> {
                 try {
-                    db.write(new Query(query)).blockLast(Duration.ofSeconds(5));
+                  db.write(new Query(query)).blockLast(Duration.ofSeconds(5));
+                } catch (ClientException ce) {
+                  if (ce.code().endsWith("EquivalentSchemaRuleAlreadyExists")) {
+                    logger.info("schema constraint already exists per cypher statement: %s", query);
+                  } else {
+                    throw ce;
+                  }
                 } catch (Exception e) {
-                    System.err.println("Could not execute " + query);
+                  logger.error("Unexpected exception during db schema assertion: %s", e.getMessage());
+                  System.exit(1);
                 }
-            });
+              });
 
-            Connection conn = UdpServer.create()
-                    .host("0.0.0.0")
-                    .port(10666)
-                    .handle((in, out) ->
-                            in.receive().asString()
-                                    //.log("RAWJSON")
-                                    .flatMap(Event::fromJson)
-                                    .windowTimeout(WINDOW_SIZE, WINDOW_DURATION)
-                                    .map(windowFlux ->
-                                            windowFlux.collectSortedList(Comparator.comparing(Event::getFrame))
-                                                    .map(Cypher::compileBulkEventComponentInsert)
-                                                    .map(query -> db.write(query).subscribe())
-                                                    .subscribe())
-                                    .then())
-                    .doOnBound(connection -> logger.info("READY!!!"))
-                    .bindNow(Duration.ofSeconds(5));
-            conn.onDispose().block();
-        }
-        logger.info("GROOM DONE!");
+      Connection conn =
+          UdpServer.create()
+              .host("127.0.0.1")
+              .port(10666)
+              .handle(
+                  (in, out) ->
+                      in.receive()
+                          .asString()
+                          .flatMap(Event::fromJson)
+                          .windowTimeout(WINDOW_SIZE, WINDOW_DURATION)
+                          .map(
+                              windowFlux ->
+                                  windowFlux
+                                      .collectSortedList(Comparator.comparing(Event::getFrame))
+                                      .filter(list -> !list.isEmpty())
+                                      .map(
+                                          eventList -> {
+                                            logger.info("processing event list with size %d", eventList.size());
+                                            return Cypher.compileBulkEventComponentInsert(
+                                                eventList);
+                                          })
+                                      .map(query -> db.write(query).subscribe())
+                                      .subscribe())
+                          .then())
+              .doOnBound(connection -> logger.info("READY FOR DATA!!!"))
+              .bindNow(Duration.ofSeconds(15));
+
+      System.in.read();
+      logger.info("GROOM SHUTTING DOWN!");
+      conn.disposeNow();
     }
+  }
 }
