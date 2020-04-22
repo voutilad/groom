76f7c5e99c3e430e998ef15be4ad7ae5044ab17b
Michael Simons <michael@simons.ac>
2020-04-22 16:32:05 +0200

    Use Micrometer and Project reactor metrics for metrics.

--- src/main/java/io/sisu/groom/GroomApplication.java
+++ src/main/java/io/sisu/groom/GroomApplication.java
@@ -1,18 +1,25 @@
 package io.sisu.groom;
 
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.dropwizard.DropwizardConfig;
+import io.micrometer.core.instrument.dropwizard.DropwizardMeterRegistry;
+import io.micrometer.core.instrument.util.HierarchicalNameMapper;
 import io.sisu.groom.events.Event;
 import io.sisu.util.BoundedArrayDeque;
 import io.sisu.util.Pair;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
 import reactor.netty.udp.UdpServer;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
 
 public class GroomApplication {
   private static final Duration WINDOW_DURATION = Duration.ofSeconds(2);
@@ -22,10 +29,12 @@ public class GroomApplication {
   private static final Logger logger;
 
   static {
-    System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
-    System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "[yyyy-MM-dd'T'HH:mm:ss:SSS]");
-    System.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
-    logger = LoggerFactory.getLogger(GroomApplication.class);
+      System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
+      System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "[yyyy-MM-dd'T'HH:mm:ss:SSS]");
+      System.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
+      logger = LoggerFactory.getLogger(GroomApplication.class);
+
+      prepareMetricSystem();
   }
 
   public static void reportPerformance(BoundedArrayDeque<Pair<Long, Integer>> queue) {
@@ -53,14 +62,6 @@ public class GroomApplication {
       // the last 6 samples at most so we get a moving average of the past ~30s or so.
       final BoundedArrayDeque<Pair<Long, Integer>> statsQueue = new BoundedArrayDeque<>(6);
 
-      Flux.interval(Duration.ofSeconds(5))
-          .map(
-              unused -> {
-                statsQueue.addFirst(new Pair(System.currentTimeMillis(), completedCnt.get()));
-                reportPerformance(statsQueue);
-                return Mono.empty();
-              })
-          .subscribe();
 
       // Where the magic happens! Listen for a UDP stream of Doom Telemetry events and
       // batch insert them into the database.
@@ -73,13 +74,16 @@ public class GroomApplication {
                       in.receive()
                           .asString()
                           .map(Event::fromJson) // Filter out invalid / unwanted elements
+                          .name("incoming_events")
+                          .metrics()
                           .onErrorContinue(Event.InvalidEventException.class, (e, o) -> logger.error("Crap event " + e.getMessage()))
                           .bufferTimeout(WINDOW_SIZE, WINDOW_DURATION) // Buffer a list of events
                           .concatMap( // Create a completely separate stream from here on, as the handler won't end due to UDPs nature
                               l -> Cypher.compileBulkEventComponentInsert(l)
                                   .flatMap(db::write)
-                                  .map(completedCnt::addAndGet)
                                   .then(db.write(Cypher.THREADING_QUERIES))
+                                  .name("stored_bulks")
+                                  .metrics()
                           )
                           .doOnComplete(() -> {
                               logger.info("Handler completed.");
@@ -88,18 +92,49 @@ public class GroomApplication {
               .doOnBound(connection -> logger.info("READY FOR DATA!!! (ctrl-c to shutdown)"))
           .bindNow(Duration.ofSeconds(15));
 
-      // Try to be kind and use a shutdown hook. This will hopefully let some data flush through.
-      Runtime.getRuntime()
-          .addShutdownHook(
-              new Thread(
-                  () -> {
-                    logger.info("WAITING UP TO 15s FOR CONNECTION TO CLOSE");
-                    conn.disposeNow(Duration.ofSeconds(15));
-                  }));
+        // Try to be kind and use a shutdown hook. This will hopefully let some data flush through.
+        Runtime.getRuntime()
+            .addShutdownHook(
+                new Thread(
+                    () -> {
+                        logger.info("WAITING UP TO 15s FOR CONNECTION TO CLOSE");
+                        conn.disposeNow(Duration.ofSeconds(15));
+                    }));
 
-      conn.onDispose().block();
-      logger.info("GROOM SHUTTING DOWN!");
-      logger.info("Received " + eventCnt.get() + " events, processed " + completedCnt.get());
+        conn.onDispose().block();
+        logger.info("GROOM SHUTTING DOWN!");
+        logger.info("Received " + eventCnt.get() + " events, processed " + completedCnt.get());
     }
   }
+
+    static void prepareMetricSystem() {
+        DropwizardConfig consoleConfig = new DropwizardConfig() {
+
+            @Override
+            public String prefix() {
+                return "console";
+            }
+
+            @Override
+            public String get(String key) {
+                return null;
+            }
+
+        };
+
+        MetricRegistry dropWizardMetricRegistry = new MetricRegistry();
+        Metrics.globalRegistry.add(
+            new DropwizardMeterRegistry(consoleConfig, dropWizardMetricRegistry, HierarchicalNameMapper.DEFAULT,
+                Clock.SYSTEM) {
+                @Override
+                protected Double nullGaugeValue() {
+                    return null;
+                }
+            });
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(dropWizardMetricRegistry)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build();
+        reporter.start(2, TimeUnit.SECONDS);
+    }
 }
