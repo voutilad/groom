b3d43129331339205124ffd69b2b564252e8af72
Dave Voutila <voutilad@gmail.com>
2020-04-23 13:02:38 -0400

    feed udp at a more digestable rate
    At least on my imac 2014 (i7-4790K CPU @ 4.00GHz), adding a fractional
    sleep helps limit how fast the packets get fired so they don't end up
    dropped (probably by the OS).
    
    Also strip newlines...need to get around to fixing that in the doom output.

--- feedudp.py
+++ feedudp.py
@@ -1,12 +1,14 @@
 import sys
+import time
 from socket import *
 cnt = 0
 s = socket(AF_INET, SOCK_DGRAM)
 with open(sys.argv[-1]) as f:
     for line in f.readlines():
         b = line.encode('ascii')
-        print('sending: ' + str(b))
+        print('sending: ' + str(b).rstrip())
         s.sendto(line.encode('ascii'), ('localhost', 10666))
         cnt = cnt + 1
+        time.sleep(0.0003)
 
 print("SENT " + str(cnt) + " events")
